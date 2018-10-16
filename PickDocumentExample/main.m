881611f38b95f408d1683ebc0c9a81660db9b859
pasha4uu <pasha7864u@gmail.com>
2018-10-16 16:54:35 +0530

    Initial Commit

---
+++ PickDocumentExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PickDocumentExample
+//
+//  Created by PASHA on 16/10/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
